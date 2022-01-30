f366ff34f7b4ff73761ce4a38d5a7cad2ee850a7
unknown <alshehri2engsam@gmail.com>
2022-01-31 01:09:31 +0300

    prodetials

--- LabProject/makeup/urls.py
+++ LabProject/makeup/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('brand/', views.brandsview, name='brand'),
     path('index/', views.dashboard, name='index'),
-    path('product/',views.prodef, name='product'),   
+    path('product/', views.prodef, name='product'),
+    path('detail/<int:id>',views.detail,name='detail'), 
+    # <int:id>
 ]
--- LabProject/makeup/views.py
+++ LabProject/makeup/views.py
@@ -15,12 +15,28 @@ def brandsview(request):
 def dashboard(request):
    # return HttpResponse("Hello, world. You're at the polls index.")
     template_name='makeup/index.html'
-    return render(request,template_name)
+    prolst=product.objects.all()
+    procount_count=prolst.count()
+    brands=brand.objects.all()
+    brand_count=brands.count()
+    context={"name":"samiah",'prolst':prolst,'procount_count':procount_count,'brand_count':brand_count}
+    return render(request,template_name,context)
+
 
 
 
 def prodef(request):
     template_name='makeup/products.html'
     prolst=product.objects.all()
-    context={"name":"samiah",'prolst':prolst}
+    procount_count=prolst.count()
+  
+    context={"name":"samiah",'prolst':prolst,'procount_count':procount_count}
     return render(request,template_name,context)
+
+
+def detail(request):
+    proditals=product.objects.all()
+    context={'product':proditals}
+    template_name='makeup/product_detials.html'
+    return render(request,template_name,context) 
+
