bd1154c36158a497de6dd4d72e652d4d0327cd0f
samiah <s.shehri@fintechsys.net>
2024-06-05 10:03:42 +0300

    edit  config

--- config/docToolchain.groovy
+++ config/docToolchain.groovy
@@ -1,16 +1,21 @@
 docToolchain {
-    inputPath = 'src/docs'
+    inputPath = 'src/docs/asciidoc'
     outputPath = 'build/docs'
     pdfOutput = 'build/docs/pdf'
     htmlOutput = 'build/docs/html5'
 
-    // Add your project-specific configurations here
-    includeTopics = ['00-introduction', '01-architecture_overview', '02-concepts']
-}
-
-templates {
-    // Specify any templates you want to use
-    html {
-        basedir = 'templates/html'
-    }
+    includeTopics = [
+        '01_introduction_and_goals',
+        '02_architecture_constraints',
+        '03_context_and_scope',
+        '04_solution_strategy',
+        '05_building_block_view',
+        '06_runtime_view',
+        '07_deployment_view',
+        '08_concepts',
+        '09_architecture_decisions',
+        '10_quality_requirements',
+        '11_technical_risks',
+        '12_glossary'
+    ]
 }
